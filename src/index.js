719b196d5a7a5eeba137fa0a3b971d0188292d4f
SusanaRamirez741 <isabel-741@hotmail.com>
2020-02-18 10:40:29 -0600

    test 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import validator from './validator.js';
 
 console.log(validator);
+const primera = document.getElementById("one");
+const segunda = document.getElementById("two");
+const tercera = document.getElementById("three");
+segunda.style.display = 'none';
+tercera.style.display = 'none';
+let boton = document.getElementById("check1");
+boton.addEventListener("click",() => {
+  segunda.style.display = "block";
+  primera.style.display = "none";
+  tercera.style.display = 'none';
+})
+let boton2 = document.getElementById("check2");
+boton2.addEventListener("click",() => {
+  segunda.style.display = "block";
+  primera.style.display = "none";
+  tercera.style.display = 'none';
+})
+let boton3 = document.getElementById("check3");
+boton3.addEventListener("click",() => {
+  tercera.style.display = "block";
+  segunda.style.display = "block";
+  let creditCardNumber = document.getElementById("play").value;
+  let output = validator.isValid(creditCardNumber);
+  let mask = validator.maskify(creditCardNumber);
+  let score = document.getElementById("validation");
+  let hola = document.getElementById("mascara")
+  hola.textContent=mask;
+  if (output==true){
+    score.textContent="Valid";
+    score.style.display="block";
+  }else{
+    score.textContent="Invalid";
+    score.style.display="block";
+  }
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
+  isValid : (creditCardNumber) => {
+
+  let sumtotal=0;
+  let invert=creditCardNumber.split('').reverse();
+  for(let i=0;i<invert.length;i++){
+    if (i%2!=0){
+      let sum=0;
+      let par=parseInt(invert[i]*2);
+      if(parseInt(par)>=10){
+      let valor=par.toString();
+      for(let j=0;j<valor.length;j++){
+        sum = sum + parseInt(valor[j]);
+        }
+       sumtotal= sumtotal + sum;
+        }
+        else{
+        sumtotal=sumtotal+parseInt(invert[i]*2);
+            }
+    }
+    else{
+    sumtotal=sumtotal+parseInt(invert[i]);
+        }
+  }
+  if(sumtotal%10==0){
+  return true;
+  }else{
+  return false;
+  }
+ },
+
+  maskify : (creditCardNumber) => {
+  let maskedString = creditCardNumber.split('');
+  for(let i = 0; i < creditCardNumber.length - 4; i++) {
+    maskedString[i] = "#";
+  }
+
+  return maskedString.join('');
+}
 };
 
 export default validator;
